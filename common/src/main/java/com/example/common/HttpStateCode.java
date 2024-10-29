c6bbd3ef1afc32626a39b550c72db3dd28594220
emiya <wzh_yjy@163.com>
2024-10-29 16:12:27 +0800

    新增更新网关token校验

---
+++ common/src/main/java/com/example/common/HttpStateCode.java
@@ -0,0 +1,28 @@
+package com.example.common;
+
+/**
+ * @author wzh
+ */
+
+public enum HttpStateCode {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String description;
+
+    HttpStateCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ common/src/main/java/com/example/common/JWTUtils.java
@@ -0,0 +1,115 @@
+package com.example.common;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JWTUtils {
+
+    /**
+     * 生成token  header.payload.singature
+     */
+
+    private static final String SING = "Loogs";
+
+    public static String getToken(Map<String, String> map) {
+
+        Calendar instance = Calendar.getInstance();
+        // 默认7天过期
+        instance.add(Calendar.DATE, 7);
+
+        //创建jwt builder
+        JWTCreator.Builder builder = JWT.create();
+
+        // payload
+        map.forEach((k, v) -> {
+            builder.withClaim(k, v);
+        });
+        String token = builder.withExpiresAt(instance.getTime())  //指定令牌过期时间
+                .sign(Algorithm.HMAC256(SING));  // sign
+        return token;
+    }
+
+    /**
+     * 验证token  合法性
+     */
+    public static DecodedJWT verify(String token) {
+        return JWT.require(Algorithm.HMAC256(SING)).build().verify(token);
+    }
+    public static Map<String, Object> checkLog(String token){
+        Map<String,Object> map = new HashMap<>();
+        try {
+            // 验证令牌
+            DecodedJWT verify = JWTUtils.verify(token);
+            map.put("state",true);
+            map.put("msg","success");
+            return map;
+        } catch (SignatureVerificationException e) {
+            e.printStackTrace();
+            map.put("msg","无效签名！");
+        }catch (TokenExpiredException e){
+            e.printStackTrace();
+            map.put("msg","token过期");
+        }catch (AlgorithmMismatchException e){
+            e.printStackTrace();
+            map.put("msg","算法不一致");
+        }catch (Exception e){
+            e.printStackTrace();
+            map.put("msg","token无效！");
+        }
+        map.put("state",false);
+        return map;
+    }
+
+
+    /**
+     * 获取token信息方法
+     */
+    public static DecodedJWT getTokenInfo(String token){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SING)).build().verify(token);
+        return verify;
+    }
+    public static Map<String, Object> checkType(String token){
+        Map<String,Object> map = new HashMap<>();
+        try {
+            // 验证令牌
+            DecodedJWT verify = JWTUtils.getTokenInfo(token);
+            map.put("type",verify.getClaim("type").asString());
+            map.put("msg","success");
+            return map;
+        } catch (SignatureVerificationException e) {
+            e.printStackTrace();
+            map.put("msg","无效签名！");
+        }catch (TokenExpiredException e){
+            e.printStackTrace();
+            map.put("msg","token过期");
+        }catch (AlgorithmMismatchException e){
+            e.printStackTrace();
+            map.put("msg","算法不一致");
+        }catch (Exception e){
+            e.printStackTrace();
+            map.put("msg","token无效！");
+        }
+        map.put("type","null");
+        return map;
+    }
+
+//    public static void main(String[] args) {
+//        Map<String, String> map = new HashMap<>();
+//        map.put("username", "loogs");
+//        map.put("type", "admin");
+//        String token = JWTUtils.getToken(map);
+//        System.out.println(token);
+//        Map map1 = JWTUtils.checkType(token);
+//        System.out.println(map1.get("type"));
+//    }
+}
